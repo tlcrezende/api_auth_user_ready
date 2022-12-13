03cb46cd92e41b1b94beacc036804e38f4cb6ca4
Thiago <tlcrezende@gmail.com>
2022-12-13 14:47:26 -0300

    Adicionado versionamento das rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
-  resources :articles
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    #scope de versionamento da api
+    # o constrains do scope vai aceitar uma classe, e se nessa classe tem um método chamado match? ele vai automaticamente chamar esse método
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do 
+      resources :articles
+    end
+    # Para avançar no versionamento, passar a v1 para false e incluir a v2 e assim vai.
+    # scope module: :v2, constrains: ApiConstraints.new(version: 1, default: true) do 
+    #   resources :articles
+    # end
+  end
 end
